60aad4e6e3a591b7daae0df68f8265f02f9c2b6c
e167115 <pineappleyuta@icloud.com>
2020-07-14 16:18:42 +0900

    Initial Commit

---
+++ BookManageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookManageApp
+//
+//  Created by 高橋佑太 on 2020/07/14.
+//  Copyright © 2020 高橋佑太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
